22b65ac43a919a16faf0c5f563929d0cde076063
KHIZERBUTT07 <153557362+KHIZERBUTT07@users.noreply.github.com>
2024-07-22 11:00:44 +0500

    Create counter.js

---
+++ class1/public/counter.js
@@ -0,0 +1,7 @@
+"use strict";
+let Batch = "SMIT-BATCH-11-MWF-9-11";
+let totalStudents = 500;
+let Active = true;
+let topicc = ["HTML", "CSS", "JAVASCRIPT"];
+// let topic: (string | number)[] = ["HTML","CSS","JAVASCRIPT",5,3];
+let weeksPerSubjects = [12, 312, 2132,];
